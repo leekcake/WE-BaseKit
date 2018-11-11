40d27fea89f8914597d2c0963d1af5251785824b
Blue Electric <fkdlxmslrx@gmail.com>
2018-11-11 17:09:11 +0900

    Move sample to standalone repo

--- sample/js/src/main/kotlin/moe/leekcake/we/sample/LaunchPoint.kt
+++
@@ -1,7 +0,0 @@
-import moe.leekcake.we.sample.SampleLeader
-import moe.leekcake.we.sample.SampleProject
-
-@JsName("main")
-fun main(args: Array<String>) {
-    SampleLeader(SampleProject())
-}
--- sample/jvm/src/main/kotlin/moe/leekcake/we/sample/BuilderLauncher.kt
+++
@@ -1,9 +0,0 @@
-package moe.leekcake.we.sample
-
-import moe.leekcake.we.basekit.Builder
-import java.io.File
-
-fun main(args: Array<String>) {
-    val builder = Builder(SampleProject())
-    builder.build( File("./result") )
-}
